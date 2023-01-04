886bf2c402d56cffeef635b03314c12fff252298
sayeedanwar648 <sayeedanwar648@gmail.com>
2023-01-04 22:11:01 +0530

    json backend created

---
+++ index.js
@@ -0,0 +1,11 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create();
+const middlewares = jsonServer.defaults();
+const router = jsonServer.router('db.json');
+
+const port = process.env.PORT;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
